04445907ff00f7157b42b2485bdbf41869f1b3e9
John Ryan <johndryan@gmail.com>
2014-01-31 18:47:43 -0500

    Added interactive prompt to bootstrap.sh for setting up extra file if it doesn't exist
    ~/.extra contains author name and email for git commits. Plan to add further items here later.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE}")"
+# ------------------------------------------
+# Setup a file with non-git-commited details
+EXTRAFILE=~/.extra
+# Check whether the ~/.extra file exists
+if [[ ! -f $EXTRAFILE ]]; then
+	# If not, proceed with prompts to set it up
+	echo 
+	echo "The $EXTRAFILE file doesn't exist. Let's make one with your personal details."
+	echo -n "Enter your author name (for git) and press [ENTER]: "
+	read name
+	# TODO: validate name
+	echo -n "Enter your email address (for git) and press [ENTER]: "
+	read email
+	# TODO: validate email address
+	# TODO: add confirmation?
+	echo
+	sed -e "s/\${name}/$name/;s/\${email}/$email/" .extra_template > $EXTRAFILE
+	echo -"Thanks! $EXTRAFILE created."
+else
+	echo "$EXTRAFILE exists. Great, we'll use it to configure git."
+fi
+# ------------------------------------------
+# Now, get on with the dotfile bootstrapping
 git pull origin master
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
